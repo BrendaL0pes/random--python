9fedb8466ee11e16407ce2508c3b4aa3099a7e97
BrendaL0pes <brendamedeiroslopes1@gmail.com>
2024-07-23 23:45:04 -0300

    Relógio Digital - versão inicial :)

---
+++ digitalclock.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root=Tk()
+root.title("Clock")
+
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000,time)
+
+label = Label(root, font=("DS_DIGIT",80),background="pink",foreground="black")
+label.pack(anchor='center')
+time()
+
+mainloop()
+
+
